c8c27256764d9671c231669f7079b577f1b7d30e
Khabir jonathan jeevan adi-nath Blok <kblok@f0r1s7.codam.nl>
2023-04-05 12:43:19 +0200

    comments

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kblok <kblok@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/03 13:08:28 by kblok         #+#    #+#                 */
-/*   Updated: 2023/04/03 17:39:14 by kblok         ########   odam.nl         */
+/*   Updated: 2023/04/05 12:41:09 by kblok         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,9 @@ int	main(int argc, char **argv, char **envp)
 {
 	return (3);
 }
+
+// Tuesdays & Wednesdays
+
+// Finish by Friday:
+// parser (look into lexer)
+// prompt
