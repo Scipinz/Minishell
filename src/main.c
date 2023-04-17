6bf11d2e4145d2502739abd4d80b9897102e4d69
Khabir jonathan jeevan adi-nath Blok <kblok@f0r1s7.codam.nl>
2023-04-17 14:32:50 +0200

    aaaaaaaa

--- src/main.c
+++ src/main.c
@@ -6,15 +6,29 @@
 /*   By: kblok <kblok@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/03 13:08:28 by kblok         #+#    #+#                 */
-/*   Updated: 2023/04/05 13:24:09 by kblok         ########   odam.nl         */
+/*   Updated: 2023/04/17 14:31:30 by kblok         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
+int	clean(bool input)
+{
+	free (input);
+	return (0);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	return (3);
+	char	*input;
+
+	while (1)
+	{
+		input = read_line_command();
+	}
+	printf("%s", input);
+	clean(true);
+	return (0);
 }
 
 // Tuesdays & Wednesdays
