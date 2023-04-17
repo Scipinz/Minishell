1796e6f0e1cdf569696a506f7281004d27cf4028
Khabir jonathan jeevan adi-nath Blok <kblok@f0r1s7.codam.nl>
2023-04-17 18:53:08 +0200

    added libs, defs, structs

--- include/shell.h
+++ include/shell.h
@@ -6,7 +6,7 @@
 /*   By: kblok <kblok@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/23 16:01:32 by kblok         #+#    #+#                 */
-/*   Updated: 2023/04/05 12:10:38 by kblok         ########   odam.nl         */
+/*   Updated: 2023/04/17 18:49:59 by kblok         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,32 @@
 
 //includes
 # include "../libft/libft.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdio.h>
+# include <stdbool.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <signal.h>
+
+//defines
+# define SPECIAL_CHARS "<>|"
 
 //structs
+typedef enum e_token_type {
+	INFILE,
+	OUTFILE,
+	COMMAND,
+	PIPE,
+	HEREDOC,
+	OUTFILE_APPEND
+} t_token_type;
+
+typedef struct s_lexer {
+	t_token_type	type;
+	struct s_lexer	*next;
+} t_lexer;
 
 #endif
