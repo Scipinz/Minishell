866fec9046cf8b459f03efd23ff40eb6fb005fb2
Khabir jonathan jeevan adi-nath Blok <kblok@f0r1s7.codam.nl>
2023-04-18 18:42:36 +0200

    Start of quote check

--- src/lexer/quotes.c
+++ src/lexer/quotes.c
@@ -6,8 +6,41 @@
 /*   By: kblok <kblok@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/17 16:51:07 by kblok         #+#    #+#                 */
-/*   Updated: 2023/04/17 16:51:14 by kblok         ########   odam.nl         */
+/*   Updated: 2023/04/18 18:42:03 by kblok         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+
+
+
+int	last_quote(char *input)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (input[i])
+	{
+		if (input[i] == '\"')
+		{
+			j = i;
+			break ;
+		}
+		i++;
+	}
+	return (j);
+}
+
+int	quote_check(int end)
+{
+	int	len;
+
+	len = 0;
+	if (end == 0)
+		return (0);
+	while (len < end)
+		len++;
+	return (len);
+}
