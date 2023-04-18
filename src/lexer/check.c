bd984ee31430610202ca44806b586dfc28dd68be
Khabir jonathan jeevan adi-nath Blok <kblok@f0r1s7.codam.nl>
2023-04-18 12:34:38 +0200

    lexer start

---
+++ src/lexer/check.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   check.c                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: kblok <kblok@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/04/17 17:02:08 by kblok         #+#    #+#                 */
+/*   Updated: 2023/04/17 18:36:26 by kblok         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shell.h"
+
+int is_special(char c)
+{
+    int i;
+
+    i = 0;
+    while (SPECIAL_CHARS[i] || c == '\"' || c == "\'")
+    {
+        if (c == SPECIAL_CHARS[i] || c == '\"' || c == '\'')
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+int symbol_length(char *input)
+{
+    int i;
+
+    i = 0;
+    while (input[i] && !ft_isspace(input[i]))
+    {
+        if ((input[i] == '<' && input[i + 1] == '<') || \
+            (input[i] == '>' && input[i + 1] == '>'))
+            return (2);
+        else
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+int lexer_length(char *input)
+{
+    int i;
+
+    i = 0;
+    while (input[i] && !ft_isspace(input[i]) && !is_special(input[i]))
+        i++;
+    return (i);
+}
---
+++ src/lexer/lexer.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   lexer.c                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: kblok <kblok@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/04/17 16:27:21 by kblok         #+#    #+#                 */
+/*   Updated: 2023/04/18 12:33:41 by kblok         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shell.h"
+
+
+int	get_type(t_token_type *type, char *input, int pos, int len)
+{
+	char	*str;
+	bool	expand;
+
+	str = ft_substr(input, pos, len);
+	if (!str)
+		return (0);
+	expand = ?(input, str);
+	if (input[pos] == '<' && expand)
+		*type = INFILE;
+	else if (input[pos] == '>' && expand)
+		*type = OUTFILE;
+	else if (input[pos] == '|' && expand)
+		*type = PIPE;
+	else if (ft_strncmp(str, ">>", 3) == 0 && expand)
+		*type = OUTFILE_APPEND;
+	else if (ft_strncmp(str, "<<", 3) == 0 && expand)
+		*type = HEREDOC;
+	else
+		*type = COMMAND;
+	free(str);
+	return (1);
+}
+
+static void	set_lexer(t_lexer **head, char *input, int len, int i)
+{
+	t_token_type	type;
+
+	if (add_token(head, input, i, len) == 0)
+		return ;
+}
+
+t_lexer	*tokens(t_lexer *head, char *input)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (input[i])
+	{
+		while (input[i] && ft_isspace(input[i]))
+			i++;
+		if (input[i] == '\"' || input[i] == "\'")
+		{
+			i++;
+			// len = quote_check(last_quote(&input[i]));
+		}
+		else if (is_special(input[i]) == 0)
+			len = lexer_length(&input[i]);
+		else
+			len = symbol_length(&input[i]);
+		if (len == 0)
+			continue ;
+		set_lexer(&head, input, len, i);
+		i += len;
+		len = 0;
+	}
+	return (head);
+}
+
+t_lexer	*lexer(char *input)
+{
+	t_lexer *head;
+
+	head = NULL;
+	head = tokens(head, input);
+}
