1485b3afdafa25def0b66dc12aee0e863c595fad
Scipinz <mrjoblok@gmail.com>
2023-06-15 18:00:28 +0200

    expandering

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -14,12 +14,50 @@
 
 char	*expanded_exit(char *input)
 {
-
+	int		i;
+	char	*exit_code;
+	
+	i = 0;
+	exit_code = NULL;
+	while (input[i])
+	{
+		if (input[i] == '$' && input[i + 1] == '?')
+		{
+			exit_code = ft_itoa(g_shell.exit_code);
+			g_shell.expanded_exit = true;
+			if (!exit_code)
+			{
+				free(exit_code);
+				return (NULL);
+			}
+		}
+		i++;
+	}
+	return (exit_code);
 }
 
 static char	*get_dollar_val(char *input)
 {
+	char	*temp;
+	int		i;
 
+	i = 0;
+	temp = ft_calloc(sizeof(char *), 1);
+	if (!temp)
+		return (NULL);
+	while ((input[i] && !ft_isspace(input[i])) \
+			&& (input[i] != '\"' && input[i] != '=') \
+			&& (ft_strncmp(input, ">>", 2) != 0 \
+			&& ft_strncmp(input, "<<", 2) != 0) \
+			&& (input[i] != '\''))
+	{
+		temp[i] = input[i];
+		i++;
+		if (input[i] == '$')
+			break ;
+	}
+	temp[i] = '\0';
+	return (temp);
 }
 
 static char	*get_env_val(char *input)
@@ -28,7 +66,21 @@ static char	*get_env_val(char *input)
 	int		i;
 
 	i = 0;
-
+	exit_code = false;
+	while (input[i])
+	{
+		if (input[i] == '$' && input[i + 1] == '?')
+			exit_code = true;
+		else if (input[i] == '$')
+		{
+			i++;
+			return (get_dollar_env(&input[i]));
+		}
+		i++;
+	}
+	if (exit_code)
+		return (expanded_exit(input));
+	return (input);
 }
 
 static char	*set_res(char *input)
